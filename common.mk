220467b9cb1d789f79c01072bec9db7be67ea065
Daniel Rosenberg <drosen@google.com>
2021-04-05 16:27:51 +0530

    sm8150-common: Enable Project ID Quota support on userdata.
     * These features enable wide inodes on userdata, which
       is required for project ID quotas. Wide inodes can
       only be set at MKFS time.
    
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -361,6 +361,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oplus
 
+# Storage
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
