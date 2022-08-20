f8c30c277f9774041258557436f237e3ee388b38
Pranav <npv12@iitbbs.ac.in>
2022-08-21 02:34:58 +0530

    sm8150-common: Drop checkpoint_gc

--- common.mk
+++ common.mk
@@ -23,19 +23,12 @@ PRODUCT_PACKAGES += \
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
     otapreopt_script
 
 # Permissions
