00ca57d8d2dc49d1890e67ed87eeae75f8eb4388
Harish <sanganiharish263@gmail.com>
2022-11-25 13:26:01 +0530

    sm8150-vommon: Drop OnePlus apps
    Signed-off-by: Harish <sanganiharish263@gmail.com>

--- common.mk
+++ common.mk
@@ -7,9 +7,6 @@
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
-# Oneplus apps
-$(call inherit-product-if-exists, vendor/oneplus/camera/camera-vendor.mk)
-
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
